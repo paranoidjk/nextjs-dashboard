a236b5d3cd714cf2a3e595cdf9e7cc7959ffb218
paranoidjk <hust2012jiangkai@gmail.com>
2024-11-09 03:22:42 +0800

    feat: without ppr to deploy

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,5 @@
-export default function Page() {
+const delay = (ms: number) => new Promise((r) => setTimeout(r, ms));
+export default async function Page() {
+    await delay(3000);
     return <p>Invoices Page</p>
 }
